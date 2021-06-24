75a0a6418bc8886331cfe63f02b0652c8b10a3e8
wenhaoxuan <jxuan@Jerrys-MacBook-Pro.local>
2021-06-24 12:59:39 -1000

    added docs for data model classes

--- interact_fit/data_model.py
+++ interact_fit/data_model.py
@@ -3,9 +3,9 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 class Data(object):
-    """Data Storage
+    """Class for input data
 
-    Stores input data
+    Stores input data from user
 
     Args:
         x (array): x data
@@ -25,6 +25,15 @@ class Data(object):
 
 
 class Model(object):
+    """Class for model
+
+    Generates a model for fitting the data. Currently supports polynomials of arbitrary degreee
+
+    Args:
+        function_type (string): type of fitting function. Options are: poly
+        init_guess (list): initial guesses for fit
+        poly_deg (int): degree of the polynomial
+    """
 
     def __init__(self, function_type, init_guess=[], poly_deg=4):
         self.poly_deg = poly_deg
@@ -41,54 +50,73 @@ class Model(object):
             self.func = self.arbitrary_poly
 
     def change_degree(self, new_degree):
+        """Change polynomial degree
+        
+        Function to change degree of the fitting polynomial
+
+        Args:
+            new_degree (int): the specified degree of polynomial
+        """
+
         self.poly_deg = new_degree
+        # update the initial guesses as well to match the degree
         self.init_guess = np.ones(self.poly_deg+1)
 
     @staticmethod
     def arbitrary_poly(x, *params):
-        # print(x)
-        # print(params)
+        """Make a polynomial function
+
+        Function called by minimization routine to create polynomial
+
+        Args:
+            x (array): array of x values to compute poly model
+            params (array): array of polynomial indices
+
+        Returns: 
+            The polynomial model
+        """
+
         return sum([p*(x**i) for i, p in enumerate(params)])
         
 class Fit(object):
+    """Class to perform a fit
+
+    Fit the data with a specified model
+
+    Args:
+        Data_object (class): a data object
+        Model_object (class): a model object
+        
+    """
 
     def __init__(self, Data_object, Model_object):
         self.data = Data_object
         self.model = Model_object
 
+        # Run a fit each time the class is instantiated
         self.y_model, self.popt, self.x_model = self.run_fit()
         
 
     def run_fit(self):
+        """Run fit
 
-        # call scipy for fit
-        # [self.data.mask]
+        Performs a fit to the data with a minimization routine, currently uses scipy curve_fit
+
+
+        Returns: 
+            y_model (array): The best fit model
+            popt (array): The best fit parameter values for the fitting function
+            x_model (array): The x array of the model
+
+        """
+
         popt, pcov = curve_fit(self.model.func, self.data.x[self.data.mask],
                              self.data.y[self.data.mask], sigma=self.data.yerr[self.data.mask],
                             p0=self.model.init_guess)
 
-        # get best fit model
+        # plot best fit model over finer x spacing
         x_model = np.linspace(np.min(self.data.x), np.max(self.data.x), 10*len(self.data.x))
         y_model = self.model.func(x_model, *popt)
 
         return y_model, popt, x_model
 
-
-    # def compute_chi2(self, Data_object):
-        
-# # Quick example
-# x_data = np.linspace(1,10,10) 
-# y_data = x_data**2 * 5 + np.random.rand(10)
-
-# # print(y_data.shape, x_data.shape)
-
-# y_err = np.ones_like(x_data) * 0.1
-
-# guess = np.zeros(4)
-# d = Data(x_data,y_data,y_err)
-# m = Model('poly', guess)
-
-# fit_obj = Fit(d, m)
-
-# print(fit_obj.y_model)
-# print(fit_obj.popt)
